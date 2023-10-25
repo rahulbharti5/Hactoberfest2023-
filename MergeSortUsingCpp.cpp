0ff0f632a2e9e32cb1d6f2c687d03c3e6ff36178
codeNinja42 <97281881+rahulbharti5@users.noreply.github.com>
2023-10-25 12:24:03 +0530

    Create MergeSortUsingCpp.cpp

---
+++ MergeSortUsingCpp.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+
+// Merge two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(std::vector<int>& arr, int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    std::vector<int> L(n1);
+    std::vector<int> R(n2);
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (int i = 0; i < n2; i++)
+        R[i] = arr[m + 1 + i];
+
+    int i = 0, j = 0, k = l;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// l is for left index and r is the right index of the sub-array of arr to be sorted
+void mergeSort(std::vector<int>& arr, int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+int main() {
+    std::vector<int> arr = {12, 11, 13, 5, 6, 7};
+    int arr_size = arr.size();
+
+    std::cout << "Original array: ";
+    for (int i = 0; i < arr_size; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    mergeSort(arr, 0, arr_size - 1);
+
+    std::cout << "\nSorted array: ";
+    for (int i = 0; i < arr_size; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    return 0;
+}
